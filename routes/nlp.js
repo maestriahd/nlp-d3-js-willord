7df159970df046ab257b2a6fa183f05a4f3da14e
Gabriel Zea <zea@gbrl.in>
2017-03-22 16:49:25 -0500

    agregado texto y actulizacion a compromise v7

--- routes/nlp.js
+++ routes/nlp.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const nlp = require('compromise');
 // textos de prueba
 const corpus = require('nlp-corpus');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   // obtiene un texto aleatorio de discursos presidenciales
-  var txt = corpus.sotu.random();
+  var txt = fs.readFileSync(__dirname+'/manifesto.txt', "utf8");
+  // var txt = corpus.sotu.random();
 
   // entrega el texto al motor de NLP
   var r = nlp(txt);
@@ -28,11 +30,11 @@ router.get('/', function(req, res) {
   res.render('nlp', {
     txt: txt,
     people: {
-      list: people.list,
+      list: people.out('array'),
       length: people.list.length
     },
     nouns: {
-      list: nouns.list,
+      list: nouns.out('array'),
       length: nouns.list.length
     }
     });
